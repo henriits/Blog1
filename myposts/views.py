e48293f64844ea52807c4cc7cc3d62d78f711fe2
henriits <121551949+henriits@users.noreply.github.com>
2023-04-16 11:14:57 +0300

    create context

--- myposts/views.py
+++ myposts/views.py
@@ -3,7 +3,30 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    context = {
+        'blog_entries': [
+            {
+                'title': 'Hello, world!',
+                'body': 'I have created my first template in Django!',
+                'date': 'Sep 27 ,2015',
+                'person': "Henri"
+
+            },
+            {
+                'title': 'Something in here',
+                'body': 'Something else in there',
+                'date': 'Sep 27, 2023',
+                'person': 'Anna'
+            },
+            {
+                'title': 'Oh Bummer!',
+                'body': 'Am i doing it right?',
+                'date': 'Sep 27, 2018',
+                'person': 'Zaid'
+            }
+        ]
+    }
+    return render(request, 'home.html', context)
 
 
 def posts(request):
