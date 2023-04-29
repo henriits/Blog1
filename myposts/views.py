5286367a1adcf805415467cd57b64a1aa4956c0e
henriits <121551949+henriits@users.noreply.github.com>
2023-04-29 13:36:50 +0300

    fix PostCreateView

--- myposts/views.py
+++ myposts/views.py
@@ -5,20 +5,17 @@ from myposts.models import Post
 # Create your views here.
 from django.contrib.auth.decorators import login_required
 
-#These are for class based views
-from django.views.generic import CreateView,ListView,UpdateView,DeleteView
-
-
+# These are for class based views
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 
 # this library is used for debugging
 import pdb
+
 
 def home(request):
     recent_post = Post.objects.all().order_by("-created_date")
 
     return render(request, 'home.html', {'posts': recent_post})
-
-
 
 
 # decorator for only authenticated user
@@ -28,7 +25,7 @@ def posts(request):
     # filter posts to list only for login  users
 
     post = Post.objects.filter(author=request.user)
-    return render(request, "posts.html",  {'posts': post})
+    return render(request, "posts.html", {'posts': post})
 
 
 # class based views
@@ -36,9 +33,8 @@ def posts(request):
 class PostCreateView(CreateView):
     model = Post
     template_name = "create_post.html"
-    fields = ["title","text",]
+    fields = ["title", "text", ]
     success_url = reverse_lazy("posts")
-
 
     def form_valid(self, form):
         form.instance.author = self.request.user
