65b385dcd85d30240acbbf74f53d084185cae060
henriits <121551949+henriits@users.noreply.github.com>
2023-04-29 13:40:14 +0300

    fix PostCreateView

--- myposts/urls.py
+++ myposts/urls.py
@@ -5,5 +5,5 @@ from .views import *
 urlpatterns = [
     path("",home, name="home"),
     path("posts/", posts, name="posts"),
-    path("create/",CreateView.as_view(), name="product_create")
+    path("create/",PostCreateView.as_view(), name="post_create")
 ]
