1446b848f9114f3410631cf112f662bc0fbbc467
henriits <121551949+henriits@users.noreply.github.com>
2023-04-29 12:45:34 +0300

    add comments

--- myposts/views.py
+++ myposts/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 from myposts.models import Post
 # Create your views here.
 from django.contrib.auth.decorators import login_required
+
+# this library is used for debugging
+import pdb
+
 def home(request):
     recent_post = Post.objects.all().order_by("-created_date")
 
@@ -11,9 +15,13 @@ def home(request):
 
 
 
+# decorator for only authenticated user
+
 @login_required()
 def posts(request):
-    post = Post.objects.all()
+    # filter posts to list only for login  users
+
+    post = Post.objects.filter(author=request.user)
     return render(request, "posts.html",  {'posts': post})
 
 
