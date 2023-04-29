6d6936d927563471b56469f2c2446eb1ee4f2ddd
henriits <121551949+henriits@users.noreply.github.com>
2023-04-29 11:55:29 +0300

    add loginrequired

--- myposts/views.py
+++ myposts/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 
 from myposts.models import Post
 # Create your views here.
+from django.contrib.auth.decorators import login_required
 def home(request):
     recent_post = Post.objects.all().order_by("-created_date")
 
     return render(request, 'home.html', {'posts': recent_post})
 
 
+
+
+@login_required()
 def posts(request):
     post = Post.objects.all()
     return render(request, "posts.html",  {'posts': post})
