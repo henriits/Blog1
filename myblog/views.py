0c955e39208df5f9a924f9c3f92bad96894c9462
henriits <121551949+henriits@users.noreply.github.com>
2023-05-14 11:12:40 +0300

    change class base to function base views (home,about)

--- myblog/views.py
+++ myblog/views.py
@@ -4,23 +4,19 @@ from django.views.generic import ListView, CreateView
 
 from myposts.forms import SignUpForm
 from myposts.models import Post
+from django.http import HttpResponse
+from django.shortcuts import render
+from myposts.models import Post
 
-class HomeView(ListView):
-    """    def get(self,request):
-        recent_post = Post.objects.all().order_by("-created_date")
 
-        return render(request, 'home.html', {'posts': recent_post})"""
-    # decorator for only authenticated user
-    model = Post
-    template_name = "home.html"
-    success_url = reverse_lazy("home")
-    context_object_name = "posts"
+def home(request):
+    featured_post = Post.objects.filter(is_featured=True)
+    recent_posts = Post.objects.all().order_by('-created_date')
+    return render(request, 'home.html', {'posts': recent_posts, 'featured_post': featured_post})
 
-class AboutView(ListView):
-    model = Post
-    template_name = 'about.html'
-    success_url = reverse_lazy("about")
-    context_object_name = "posts"
+def about(request):
+    return render(request, 'about.html')
+
 
 
 class UserCreateView(CreateView):
